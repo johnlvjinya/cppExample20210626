2caa8ee12e76f926cd7bf2210240ca48a69c52be
johnlvjinya <1040635691@qq.com>
2021-06-27 22:53:14 +0800

    add

--- s00_main.cpp
+++ s00_main.cpp
@@ -3,6 +3,7 @@
 #include "s01_csv_write_by_csv2.h"
 #include "s02_csv_read_by_csv2.h"
 #include "s03_or_tools_test01.h"
+#include "s04_prime_number_cost_time.h"
 
 //push test
 int main()
@@ -10,7 +11,8 @@ int main()
 	//csv_write01();
 	//csv_write02();
 	//main_read();
-	main_json_test01();
+	//main_json_test01();
+	prime_number(10000000);
 
 	return 0;
 }
