4dbbab049f2b9d5552010bf7578acd698089a16b
johnlvjinya <1040635691@qq.com>
2021-06-27 21:54:38 +0800

    add json test

--- s00_main.cpp
+++ s00_main.cpp
@@ -2,13 +2,15 @@
 #include "s01_csv_write.h"
 #include "s01_csv_write_by_csv2.h"
 #include "s02_csv_read_by_csv2.h"
+#include "s03_or_tools_test01.h"
 
 //push test
 int main()
 {
 	//csv_write01();
 	//csv_write02();
-	main_read();
+	//main_read();
+	main_json_test01();
 
 	return 0;
 }
